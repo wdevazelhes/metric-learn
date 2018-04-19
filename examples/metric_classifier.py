874e95a2ca5a479c332879d2d6d751361e177d40
William de Vazelhes <william.de-vazelhes@inria.fr>
2018-04-19 17:27:32 +0200

    wip

---
+++ examples/metric_classifier.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from metric_learn import LSML, MetricClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from metric_learn.sampling import QuadrupletSampler
+
+
+RNG_SEED = 0
+
+iris = load_iris()
+n_samples, n_features = iris.data.shape
+
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target,
+                                                    test_size=0.2,
+                                                    random_state=RNG_SEED)
+
+clf = MetricClassifier(metric_learner=LSML(),
+                       classifier=KNeighborsClassifier(),
+                       sampler=QuadrupletSampler(n_samples=1000,
+                                                 with_replacement=True,
+                                                 random_state=RNG_SEED))
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print(classification_report(y_test, y_pred))
+
+#
+# Learned metric space visualization
+#
+
+pca_test = PCA(n_components=2).fit_transform(clf.transform(X_test))
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.scatter(pca_test[:, 0], pca_test[:, 1], c=y_test, marker='x',
+           label='test')
+ax.legend(loc='best')
+plt.show()
+
